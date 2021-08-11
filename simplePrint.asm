9b88827933ca99da5d9881038034ef00d444f1ec
Bhavya Goel <bgoel4132@gmail.com>
2021-08-12 01:30:33 +0530

    test: printing simple messages

--- simplePrint.asm
+++ simplePrint.asm
@@ -2,7 +2,7 @@
 ; Writes "Hello, World" to the console using only system calls. Runs on 64-bit Linux only.
 ; To assemble and run:
 ;
-;     nasm -felf64 hello.asm && ld hello.o && ./a.out
+;     nasm -f elf64 hello.asm && ld hello.o && ./a.out
 ; ----------------------------------------------------------------------------------------
 
           global    _start
@@ -13,9 +13,17 @@ _start:   mov       rax, 1                  ; system call for write
           mov       rsi, message            ; address of string to output
           mov       rdx, 13                 ; number of bytes
           syscall                           ; invoke operating system to do the write
+ 
+          mov       rax, 1
+          mov       rdi, 1
+          mov       rsi, message2
+          mov       rdx, 13
+          syscall
           mov       rax, 60                 ; system call for exit
           xor       rdi, rdi                ; exit code 0
           syscall                           ; invoke operating system to exit
 
+
           section   .data
-message:  db        "Hello, World", 10      ; note the newline at the end
+message:  dw        "Hello, World", 10      ; note the newline at the end
+message2: dw        "Bhavya Goel", 10
